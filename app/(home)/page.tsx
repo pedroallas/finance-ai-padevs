7f693a4e27f81fe22021e74e159dcf1ef0e38edc
pedroallas <pedro.borges@faculdadegran.edu.br>
2026-01-28 00:07:00 -0300

    feat: add last transactions components

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -7,6 +7,7 @@ import { isMatch } from "date-fns";
 import TransactionsPieChart from "./_components/transactions-pie-chart";
 import { getDashboard } from "../_data/get-dashboard";
 import ExpensesPerCategory from "./_components/expenses-per-category";
+import LastTransactions from "./_components/last-transactions";
 
 interface HomeProps {
   searchParams: {
@@ -42,6 +43,7 @@ const HomePage = async ({ searchParams: { month } }: HomeProps) => {
               />
             </div>
           </div>
+          <LastTransactions lastTransactions={dashboard.lastTransactions} />
         </div>
       </div>
     </>
