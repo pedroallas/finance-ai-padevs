6779d1b0407325fd365c8c066c1b545d6c6ff08c
pedroallas <pedro.borges@faculdadegran.edu.br>
2026-01-29 00:33:10 -0300

    feat: add stripe webhook that handles subscriptions

---
+++ app/api/webhooks/stripe/route.ts
@@ -0,0 +1,76 @@
+import { clerkClient } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+import Stripe from "stripe";
+
+export const POST = async (request: Request) => {
+  if (!process.env.STRIPE_SECRET_KEY || !process.env.STRIPE_WEBHOOK_SECRET) {
+    return NextResponse.error();
+  }
+  const signature = request.headers.get("stripe-signature");
+  if (!signature) {
+    return NextResponse.error();
+  }
+  const text = await request.text();
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+    apiVersion: "2024-10-28.acacia",
+  });
+  const event = stripe.webhooks.constructEvent(
+    text,
+    signature,
+    process.env.STRIPE_WEBHOOK_SECRET,
+  );
+
+  switch (event.type) {
+    case "invoice.paid": {
+      // Pega o primeiro item da fatura
+      const invoice = event.data.object as Stripe.Invoice;
+      const customer = invoice.customer as string;
+      const lineItem = invoice.lines.data[0];
+      const clerkUserId = lineItem?.metadata?.clerk_user_id;
+      // Faz um cast explícito para acessar o campo parent sem usar 'any'
+      const parent = (
+        lineItem as unknown as {
+          parent?: {
+            subscription_item_details?: {
+              subscription?: string;
+            };
+          };
+        }
+      ).parent;
+      const subscription = parent?.subscription_item_details?.subscription;
+      if (!clerkUserId || !subscription) {
+        return NextResponse.error();
+      }
+      await clerkClient().users.updateUser(clerkUserId, {
+        privateMetadata: {
+          stripeCustomerId: customer,
+          stripeSubscriptionId: subscription,
+        },
+        publicMetadata: {
+          subscriptionPlan: "premium",
+        },
+      });
+      break;
+    }
+    case "customer.subscription.deleted": {
+      const subscription = await stripe.subscriptions.retrieve(
+        (event.data.object as { id: string }).id,
+      );
+      const clerkUserId = subscription.metadata?.clerk_user_id;
+      if (!clerkUserId) {
+        return NextResponse.error();
+      }
+      await clerkClient().users.updateUser(clerkUserId, {
+        privateMetadata: {
+          stripeCustomerId: null,
+          stripeSubscriptionId: null,
+        },
+        publicMetadata: {
+          subscriptionPlan: null,
+        },
+      });
+      break;
+    }
+  }
+  return NextResponse.json({ received: true });
+};
